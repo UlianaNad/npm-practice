9db624d091ac97f6aef616efa9e89902eaaef0d2
”John <102276922+UlianaNad@users.noreply.github.com>
2022-07-22 23:31:24 +0300

    change22.07

--- main.js
+++ main.js
@@ -4,19 +4,25 @@ const fs  = require('fs');
 
 
 
-const timestamp = Date.now();
-console.log(timestamp);
+
 
 const getPeople = async () => {
     let result = await axios.get(`https://swapi.dev/api/people/3/`);
-    console.log(result);
-   
-    fs.writeFile("people.txt", data, (err) =>{
+        console.log(result);
+
+    fs.writeFile("test.txt", JSON.stringify(result.data), (err) =>{
         if (err){
             console.log(err);
         }else{
             console.log("Succesfully");
         }
+    });
+
+    const timestamp = Date.now();
+        console.log(timestamp);
+
+    fs.rename('test.txt', '`${timestamp}`.txt', () => {
+        console.log("It works");
     });
 
 };
